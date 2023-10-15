375bd5d9e1349d36c9cfaf032fd8c9154876b62c
filipkrw <filipkrw.dev@gmail.com>
2023-10-15 23:09:20 +0100

    Revert "Look ahead behavior"
    This reverts commit 4e1968ac2badf97c0fdc9ca0c95ce05fbd59f28a.

--- src/kinematic/align/KinematicAlign.h
+++ src/kinematic/align/KinematicAlign.h
@@ -3,8 +3,11 @@
 
 #include "../common/KinematicSteering.h"
 
-typedef struct KinematicAlignParams
+class KinematicAlign : public KinematicSteering
 {
+public:
+    KinematicAlign(KinematicEntity *character) : KinematicSteering(character){};
+
     double maxAngularAcceleration = 3;
     double maxRotation = 1.5;
 
@@ -12,33 +15,6 @@ typedef struct KinematicAlignParams
     double slowRadius = 0.5;
 
     double timeToTarget = 0.1;
-} KinematicAlignParams;
-
-class KinematicAlign : public KinematicSteering
-{
-public:
-    KinematicAlign(KinematicEntity *character) : KinematicSteering(character)
-    {
-        KinematicAlignParams params = KinematicAlignParams();
-        KinematicAlign(character, &params);
-    };
-
-    KinematicAlign(KinematicEntity *character, KinematicAlignParams *params) : KinematicSteering(character)
-    {
-        this->maxAngularAcceleration = params->maxAngularAcceleration;
-        this->maxRotation = params->maxRotation;
-        this->targetRadius = params->targetRadius;
-        this->slowRadius = params->slowRadius;
-        this->timeToTarget = params->timeToTarget;
-    };
-
-    double maxAngularAcceleration;
-    double maxRotation;
-
-    double targetRadius;
-    double slowRadius;
-
-    double timeToTarget;
 
     void CalculateSteering(double orientation);
 };
--- src/kinematic/arrive/KinematicArriveDemo.h
+++ src/kinematic/arrive/KinematicArriveDemo.h
@@ -5,30 +5,18 @@
 #include "../../GameWorld.h"
 #include "../common/KinematicEntity.h"
 #include "KinematicArrive.h"
-#include "../look-ahead/LookAhead.h"
 
 class KinematicArriveDemo : public Demo
 {
 private:
     KinematicEntity *entity;
     KinematicArrive *arrive;
-    LookAhead *lookAhead;
 
 public:
     KinematicArriveDemo() : Demo()
     {
         entity = new KinematicEntity(Vector2(600, 500));
         arrive = new KinematicArrive(entity);
-
-        KinematicAlignParams *lookAheadParams = new KinematicAlignParams{
-            .maxAngularAcceleration = 1000,
-            .maxRotation = 7.5,
-            .targetRadius = 0.02,
-            .slowRadius = 0.8,
-            .timeToTarget = 0.01,
-        };
-
-        lookAhead = new LookAhead(entity, lookAheadParams);
     };
 
     void Update(double dt)
@@ -36,9 +24,6 @@ public:
         Vector2 crosshairPosition = gameWorld->GetCrosshair()->Position();
         arrive->CalculateSteering(crosshairPosition);
         entity->Update(arrive->steering, dt);
-
-        lookAhead->CalculateSteering();
-        entity->Update(lookAhead->steering, dt);
     }
 
     void Render()
